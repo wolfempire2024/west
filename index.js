e771c45cbf640b0ee6f8ef3fee90318c35b0d6c1
wolfempire2024 <wolfempire2024@gmail.com>
2024-10-30 19:06:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,20 @@ var opt = {
 const cooldown = new Set();
 
 
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
+
+
 client.on("ready",async () => {
 
 console.log(`${client.user.username} Is Ready Now .`)
